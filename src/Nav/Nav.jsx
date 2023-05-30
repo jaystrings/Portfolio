a975875503d5de27c3f06a15f18ae6b30dcf97ef
jaystrings <johnny.adejumo6@gmail.com>
2023-05-30 11:10:19 +0100

    third commit

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,23 +1,71 @@
-import React from 'react'
-import './nav.css'
-import {AiOutlineHome} from 'react-icons/ai'
-import {AiOutlineUser} from 'react-icons/ai'
-import { BiAbacus, BiBookContent } from 'react-icons/bi'
-import {RiServiceLine} from 'react-icons/ri'
-import {BiMessageSquareDetail} from 'react-icons/bi'
-import { useState } from 'react'
+import React from "react";
+import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiAbacus, BiBookContent } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { BiMessageSquareDetail } from "react-icons/bi";
+import { useState } from "react";
 function Nav() {
-  const [active, setActive] = useState('#')
+  const [active, setActive] = useState("#");
   return (
     <nav>
-      <a href="#" onClick={()=>setActive('#')} className={active ==='#'? 'active':''}><AiOutlineHome/></a>
-     <a href ='#about' onClick={()=>setActive('#about')} className={active ==='#about'? 'active':''}><AiOutlineUser/></a>
-     <a href ='#portfolio' onClick={()=>setActive('#portfolio')} className={active ==='#portfolio'? 'active':''}><BiAbacus/></a>
-     <a href ='#experience' onClick={()=>setActive('#experience')} className={active ==='#experience'? 'active':''}><BiBookContent/></a>
-     <a href ='#services' onClick={()=>setActive('#services')} className={active ==='#services'? 'active':''}><RiServiceLine/></a>
-     <a href ='#contact'onClick={()=>setActive('#contact')} className={active ==='#contact'? 'active':''}><BiMessageSquareDetail/></a>
+      <a
+        href="#"
+        onClick={() => setActive("#")}
+        className={active === "#" ? "active" : ""}
+        without
+        rel="noreferrer"
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActive("#about")}
+        className={active === "#about" ? "active" : ""}
+        without
+        rel="noreferrer"
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#portfolio"
+        onClick={() => setActive("#portfolio")}
+        className={active === "#portfolio" ? "active" : ""}
+        without
+        rel="noreferrer"
+      >
+        <BiAbacus />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActive("#experience")}
+        className={active === "#experience" ? "active" : ""}
+        without
+        rel="noreferrer"
+      >
+        <BiBookContent />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActive("#services")}
+        className={active === "#services" ? "active" : ""}
+        without
+        rel="noreferrer"
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActive("#contact")}
+        className={active === "#contact" ? "active" : ""}
+        without
+        rel="noreferrer"
+      >
+        <BiMessageSquareDetail />
+      </a>
     </nav>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
