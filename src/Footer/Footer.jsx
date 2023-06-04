3cc527d6a5a3502bf3159036f3062db1fc98b376
jaystrings <johnny.adejumo6@gmail.com>
2023-06-04 17:53:13 +0100

    changed about status

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <a href="#" className='footer__logo'>Adejumo John</a>
     <ul className='permalinks'>
         <li><a href="#">Home</a></li>
-        <li><a href="about">About</a></li>
+        <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#services">Services</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
